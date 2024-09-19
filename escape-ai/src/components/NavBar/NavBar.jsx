d262cc0a6a2aea36874f217c53db7b689bdb5540
Brachie Erentroy <145331020+brachaer@users.noreply.github.com>
2024-09-19 09:38:49 +0300

    fix : use rout instead of sucket

--- escape-ai/src/components/NavBar/NavBar.jsx
+++ escape-ai/src/components/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ import ToggleThemeButton from "../Theme/ToggleThemeButton";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 import LogoImage from "../LogoImage/LogoImage";
 import { useTheme } from "@mui/material/styles";
+import img from "../../assets/escape-logo.jpeg";
 
 const NavBar = () => {
   const theme = useTheme();
@@ -18,7 +19,7 @@ const NavBar = () => {
       }}
     >
       <Toolbar variant="dense">
-        <LogoImage src="../src/assets/escape-logo.jpeg" width="8%" />
+        <LogoImage src={img} width="8%" />
         <Typography
           variant="h3"
           component="div"
